e90a921a088f23e89c81f80a4be526296216fead
AntonyMadaleno <55442410+AntonyMadaleno@users.noreply.github.com>
2023-02-06 22:48:43 +0100

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -8,5 +8,21 @@ function loadCards()
         cards.push( document.createElement("div") );
         cards[i].setAttribute("class", "card");
         elm_card_container.appendChild(cards[i]);
+
+        let text;
+        fig = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "Q", "K"];
+
+        if ( i / 13 < 1)
+            text = document.createTextNode("CO " + fig[i%13] );
+        else if ( i / 13 < 2)
+            text = document.createTextNode("PI " + fig[i%13] );
+        else if ( i / 13 < 3)
+            text = document.createTextNode("TR " + fig[i%13] );
+        else if ( i / 13 < 4)
+            text = document.createTextNode("CA " + fig[i%13] );
+        else
+            text = document.createTextNode("JO " + fig[i%13] );
+
+        cards[i].appendChild(text);
     }
 }
